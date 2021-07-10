5a2282840742c3e3ac44c147c532f6ffe5fa87cb
Sean <sean@vestaridefair.com>
2021-07-09 22:08:47 -0700

    adjusting my styles

---
+++ myDraft/src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+// Components
+import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
+import SearchBar from './SearchBar';
+import Button from './Button';
+
+// Hook
+import { useHomeFetch } from '../hooks/useHomeFetch';
+// Image
+import NoImage from '../images/no_image.jpg';
+
+const Home = () => {
+
+  return (
+    <>
+      <h1 style={{ "color": "red" }}>ME</h1>
+    </>
+  );
+};
+
+export default Home;
---
+++ myDraft/src/components/Movie.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+// Components
+import BreadCrumb from './BreadCrumb';
+import Grid from './Grid';
+import Spinner from './Spinner';
+import MovieInfo from './MovieInfo';
+import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
+// Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
+// Image
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+
+  return (
+    <>
+      <h1 style={{ "color": "red" }}>Movie page</h1>
+    </>
+  );
+};
+
+export default Movie;
